f7267f9cdb8dc3eb16aac0f43e67bd1d71bd0c3d
toilavic <t18042k@gmail.com>
2021-10-27 16:09:07 +0300

    init backend with installed dependencies

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# haircut-app
+## Available Scripts
+
+`cd backend/ && npm i && nodemon index.js` to install and run app manually
+
+# PREVIEW
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view frontend in the browser.
+
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const middleware = require("./utils/middleware");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+// const itemsRouter = require("./controllers/items");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(express.static("docs"));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World API!");
+});
+
+// app.use("/api/items", itemsRouter);
+
+
+app.use(middleware.unknownEndpoint); // handles unkown endpoints
+app.use(middleware.errorHandler); // handles known errors
+
+module.exports = app;
