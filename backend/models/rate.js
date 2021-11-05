2561edaa1ed3b4fbe72f405955622ed4627833d5
Thinh Duc <t9ngdu00@students.oamk.fi>
2021-11-05 11:30:02 +0200

    updated

--- backend/models/rate.js
+++ backend/models/rate.js
@@ -31,7 +31,7 @@ const rateSchema = new mongoose.Schema({
 rateSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
+        // delete returnedObject._id;
         delete returnedObject.__v;
     }
 });
